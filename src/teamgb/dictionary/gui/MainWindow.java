024adaf8469b904967390119b264a05b306bbce2
gilz688 <eromlig.ac@gmail.com>
2013-09-16 23:26:16 +0800

    update

--- src/teamgb/dictionary/gui/MainWindow.java
+++ src/teamgb/dictionary/gui/MainWindow.java
@@ -64,6 +64,7 @@ public class MainWindow {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
